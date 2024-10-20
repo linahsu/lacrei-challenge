4dbf8c540a48516e3034e7c251cb057285398db8
Lina Hsu <linahthsu@gmail.com>
2024-10-20 02:53:30 -0300

    refactor: adds waitFor function to the Quem Somos button test to ensure the window.open

--- tests/homePage.test.tsx
+++ tests/homePage.test.tsx
@@ -1,19 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HomePage from '../app/page';
 
 describe('HomePage', () => {
-beforeEach(() => {
-jest.spyOn(window, 'open').mockImplementation(() => null);
-});
+    beforeEach(() => {
+        window.open = jest.fn();
+    });
 
-afterEach(() => {
-jest.clearAllMocks();
-});
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-test('Should open the correct URL when the "Quem Somos" button is clicked', () => {
-render(<HomePage />);
-userEvent.click(screen.getByText('Quem Somos'));
-expect(window.open).toHaveBeenCalledWith('https://lacreisaude.com.br/quem-somos/', 'blank');
-});
+    test('Should open the correct URL when the "Quem Somos" button is clicked', async () => {
+        render(<HomePage />);
+
+        const aboutButton = screen.getByText('Quem Somos');
+        
+        userEvent.click(aboutButton);
+        
+        await waitFor(() => {
+            expect(window.open).toHaveBeenCalledWith('https://lacreisaude.com.br/quem-somos/', 'blank');
+        });
+    });
 });
